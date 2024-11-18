ca83dfe2cf6e3c64f938b29808cf566efae743e8
Luis Fernando Reyes Betancourt <150478475+FernandoReyes04@users.noreply.github.com>
2024-11-18 14:43:15 -0600

    Proyecto finalizado

---
+++ DataBinding/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.Maui.Controls;
+
+
+namespace DataBinding
+{
+
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+            GetData();
+        }
+
+        private readonly HttpClient _httpClient = new HttpClient();
+
+        private ObservableCollection<PersonaModel> _personas;
+        public ObservableCollection<PersonaModel> Personas
+        {
+            get => _personas;
+            set
+            {
+                _personas = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public async void GetData()
+        {
+            var response = await _httpClient
+                .GetStringAsync("https://fi.jcaguilar.dev/v1/escuela/persona");
+            var personas = JsonSerializer
+                .Deserialize<ObservableCollection<PersonaModel>>(response);
+
+            Personas = personas;
+        }
+
+        private async void btnpagsig_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new FormPage());
+        }
+    }
+
+}
---
+++ DataBinding/PersonaModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBinding
+{
+    public class PersonaModel
+    {
+        public int id_persona { get; set; } // ID único para la persona
+
+        public string nombre { get; set; } // Nombre de la persona
+
+        public string apellido { get; set; } // Apellido de la persona
+
+        public string sexo { get; set; } // Sexo: "h" (hombre) o "m" (mujer)
+
+        public DateTime fh_nac { get; set; } // Fecha de nacimiento
+
+        public int id_rol { get; set; } // ID del rol asociado
+    }
+}
